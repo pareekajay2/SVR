48027b996c4b15d4ad528167e8165fe5153085fa
Ajay Pareek <39679557+pareekajay2@users.noreply.github.com>
2018-06-17 15:08:19 +0530

    Add files via upload

---
+++ Position_Salaries.R
@@ -0,0 +1,21 @@
+dataset = read.csv('Position_Salaries.csv')
+dataset = dataset[2:3]
+
+install.packages('e1071')
+library(e1071)
+regressor = svm(formula = Salary ~ .,
+                data = dataset,
+                type = 'eps-regression',
+                kernel = 'radial')
+
+y_pred = predict(regressor, data.frame(Level = 6.5))
+
+x_grid = seq(min(dataset$Level), max(dataset$Level), 0.01)
+ggplot() +
+  geom_point(aes(x = dataset$Level, y = dataset$Salary),
+             colour = 'red') +
+  geom_line(aes(x = x_grid, y = predict(regressor, newdata = data.frame(Level = x_grid))),
+            colour = 'blue') +
+  ggtitle('Salary vs Level(SVR)') +
+  xlab('Level') +
+  ylab('Salary')
